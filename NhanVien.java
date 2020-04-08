58a2f0cbe9bae25a2f5088dafedf7a41609089bf
meocutee123 <49399040+meocutee123@users.noreply.github.com>
2020-04-08 10:43:13 +0700

    add code lines

--- NhanVien.java
+++ NhanVien.java
@@ -9,15 +9,20 @@ public class NhanVien {
         Name = "Nghia";
         Age = 20;
         Address = "664/2 2/4 street";
-        Salary = 1000;
+        Salary = 1000d;
         workHours = 40;
     }
     public String getThongTin(){
-        return Name + Age + Address + Salary + workHours;
+        return Name + " " + Age + " " + Address + " " + Salary + " " + workHours;
+    }
+    public double tinhThuong() {
+        if (workHours >= 200) return Salary * 0.2;
+        else if (workHours >= 100 && workHours < 200) return Salary * 0.1;
+        else return 0;
     }
     public static void main(String[] args) {
         NhanVien Infor = new NhanVien();
-       System.out.println(Infor.getThongTin());
-        
+        System.out.println(Infor.getThongTin());
+        System.out.println(Infor.tinhThuong());
     }
 }
